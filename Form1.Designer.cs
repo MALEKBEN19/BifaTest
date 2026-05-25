e803ef7cca04faed2294e315b3daa8c5448de6eb
Abdelmalek Bensidhome <abdelmalek.bensidhoum@hotmail.com>
2026-05-25 13:12:49 +0100

    Update UI layout and add budget/employee tracking

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tpMenu = new System.Windows.Forms.TabPage();
             this.tpAddEmployee = new System.Windows.Forms.TabPage();
-            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.pbIdentity = new System.Windows.Forms.PictureBox();
             this.btnAddEmployee = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -51,20 +51,25 @@
             this.txbLastName = new System.Windows.Forms.TextBox();
             this.txbFirstName = new System.Windows.Forms.TextBox();
             this.tpEmployeeList = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.tpBudget = new System.Windows.Forms.TabPage();
+            this.label7 = new System.Windows.Forms.Label();
             this.btnNext = new System.Windows.Forms.Button();
             this.btnPerivous = new System.Windows.Forms.Button();
             this.lblUser = new System.Windows.Forms.Label();
             this.btnLoginLogout = new System.Windows.Forms.Button();
             this.epMenu = new System.Windows.Forms.ErrorProvider(this.components);
             this.lbDate = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotalBudget = new System.Windows.Forms.Label();
+            this.lblTotalEmployees = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tpAddEmployee.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbIdentity)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.tpEmployeeList.SuspendLayout();
+            this.tpBudget.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.epMenu)).BeginInit();
             this.SuspendLayout();
             // 
@@ -74,6 +79,7 @@
             this.tabControl1.Controls.Add(this.tpAddEmployee);
             this.tabControl1.Controls.Add(this.tpEmployeeList);
             this.tabControl1.Controls.Add(this.tpBudget);
+            this.tabControl1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(30, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -84,10 +90,10 @@
             // 
             this.tpMenu.BackgroundImage = global::BifaProject.Properties.Resources.bifo;
             this.tpMenu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.tpMenu.Location = new System.Drawing.Point(4, 22);
+            this.tpMenu.Location = new System.Drawing.Point(4, 28);
             this.tpMenu.Name = "tpMenu";
             this.tpMenu.Padding = new System.Windows.Forms.Padding(10);
-            this.tpMenu.Size = new System.Drawing.Size(1075, 500);
+            this.tpMenu.Size = new System.Drawing.Size(1075, 494);
             this.tpMenu.TabIndex = 0;
             this.tpMenu.Text = "Menu";
             this.tpMenu.UseVisualStyleBackColor = true;
@@ -95,7 +101,7 @@
             // tpAddEmployee
             // 
             this.tpAddEmployee.BackColor = System.Drawing.Color.Beige;
-            this.tpAddEmployee.Controls.Add(this.label7);
+            this.tpAddEmployee.Controls.Add(this.label9);
             this.tpAddEmployee.Controls.Add(this.pbIdentity);
             this.tpAddEmployee.Controls.Add(this.btnAddEmployee);
             this.tpAddEmployee.Controls.Add(this.groupBox1);
@@ -110,21 +116,22 @@
             this.tpAddEmployee.Controls.Add(this.txbAddress);
             this.tpAddEmployee.Controls.Add(this.txbLastName);
             this.tpAddEmployee.Controls.Add(this.txbFirstName);
-            this.tpAddEmployee.Location = new System.Drawing.Point(4, 22);
+            this.tpAddEmployee.Location = new System.Drawing.Point(4, 28);
             this.tpAddEmployee.Name = "tpAddEmployee";
             this.tpAddEmployee.Padding = new System.Windows.Forms.Padding(3);
-            this.tpAddEmployee.Size = new System.Drawing.Size(1075, 500);
+            this.tpAddEmployee.Size = new System.Drawing.Size(1075, 494);
             this.tpAddEmployee.TabIndex = 1;
             this.tpAddEmployee.Text = "Add Employees";
             // 
-            // label7
+            // label9
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(38, 20);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "label7";
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(412, 14);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(270, 41);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Add Employees";
             // 
             // pbIdentity
             // 
@@ -145,7 +152,7 @@
             this.btnAddEmployee.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.btnAddEmployee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAddEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddEmployee.Location = new System.Drawing.Point(488, 286);
+            this.btnAddEmployee.Location = new System.Drawing.Point(459, 286);
             this.btnAddEmployee.Name = "btnAddEmployee";
             this.btnAddEmployee.Size = new System.Drawing.Size(181, 54);
             this.btnAddEmployee.TabIndex = 5;
@@ -158,7 +165,7 @@
             this.groupBox1.Controls.Add(this.rbFemale);
             this.groupBox1.Controls.Add(this.rbMale);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(439, 109);
+            this.groupBox1.Location = new System.Drawing.Point(419, 126);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(288, 123);
             this.groupBox1.TabIndex = 4;
@@ -193,7 +200,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(49, 349);
+            this.label6.Location = new System.Drawing.Point(22, 349);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(79, 25);
             this.label6.TabIndex = 3;
@@ -203,7 +210,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(49, 269);
+            this.label5.Location = new System.Drawing.Point(22, 269);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(70, 25);
             this.label5.TabIndex = 3;
@@ -213,7 +220,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(49, 189);
+            this.label4.Location = new System.Drawing.Point(22, 189);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 25);
             this.label4.TabIndex = 3;
@@ -233,7 +240,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(49, 109);
+            this.label2.Location = new System.Drawing.Point(22, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(124, 25);
             this.label2.TabIndex = 3;
@@ -243,7 +250,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(49, 29);
+            this.label1.Location = new System.Drawing.Point(22, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 25);
             this.label1.TabIndex = 2;
@@ -253,7 +260,7 @@
             // 
             this.mkbSalary.BeepOnError = true;
             this.mkbSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mkbSalary.Location = new System.Drawing.Point(54, 377);
+            this.mkbSalary.Location = new System.Drawing.Point(27, 377);
             this.mkbSalary.Mask = "\\D\\A 000000";
             this.mkbSalary.Name = "mkbSalary";
             this.mkbSalary.Size = new System.Drawing.Size(330, 31);
@@ -262,7 +269,7 @@
             // txbEmail
             // 
             this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
-            this.txbEmail.Location = new System.Drawing.Point(54, 297);
+            this.txbEmail.Location = new System.Drawing.Point(27, 297);
             this.txbEmail.Name = "txbEmail";
             this.txbEmail.Size = new System.Drawing.Size(330, 32);
             this.txbEmail.TabIndex = 0;
@@ -270,7 +277,7 @@
             // txbAddress
             // 
             this.txbAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
-            this.txbAddress.Location = new System.Drawing.Point(54, 217);
+            this.txbAddress.Location = new System.Drawing.Point(27, 217);
             this.txbAddress.Name = "txbAddress";
             this.txbAddress.Size = new System.Drawing.Size(330, 32);
             this.txbAddress.TabIndex = 0;
@@ -278,7 +285,7 @@
             // txbLastName
             // 
             this.txbLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
-            this.txbLastName.Location = new System.Drawing.Point(54, 137);
+            this.txbLastName.Location = new System.Drawing.Point(27, 137);
             this.txbLastName.Name = "txbLastName";
             this.txbLastName.Size = new System.Drawing.Size(330, 32);
             this.txbLastName.TabIndex = 0;
@@ -286,7 +293,7 @@
             // txbFirstName
             // 
             this.txbFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
-            this.txbFirstName.Location = new System.Drawing.Point(54, 57);
+            this.txbFirstName.Location = new System.Drawing.Point(27, 57);
             this.txbFirstName.Name = "txbFirstName";
             this.txbFirstName.Size = new System.Drawing.Size(330, 32);
             this.txbFirstName.TabIndex = 0;
@@ -296,22 +303,55 @@
             this.tpEmployeeList.BackColor = System.Drawing.Color.Beige;
             this.tpEmployeeList.Controls.Add(this.label8);
             this.tpEmployeeList.Controls.Add(this.listView1);
-            this.tpEmployeeList.Location = new System.Drawing.Point(4, 22);
+            this.tpEmployeeList.Location = new System.Drawing.Point(4, 28);
             this.tpEmployeeList.Name = "tpEmployeeList";
             this.tpEmployeeList.Padding = new System.Windows.Forms.Padding(3);
-            this.tpEmployeeList.Size = new System.Drawing.Size(1075, 500);
+            this.tpEmployeeList.Size = new System.Drawing.Size(1075, 494);
             this.tpEmployeeList.TabIndex = 2;
             this.tpEmployeeList.Text = "Employees List";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(401, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(264, 41);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Employees List";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 71);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1063, 417);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // tpBudget
             // 
             this.tpBudget.BackColor = System.Drawing.Color.Beige;
-            this.tpBudget.Location = new System.Drawing.Point(4, 22);
+            this.tpBudget.Controls.Add(this.lblTotalEmployees);
+            this.tpBudget.Controls.Add(this.lblTotalBudget);
+            this.tpBudget.Controls.Add(this.label7);
+            this.tpBudget.Controls.Add(this.button1);
+            this.tpBudget.Location = new System.Drawing.Point(4, 28);
             this.tpBudget.Name = "tpBudget";
             this.tpBudget.Padding = new System.Windows.Forms.Padding(3);
-            this.tpBudget.Size = new System.Drawing.Size(1075, 500);
+            this.tpBudget.Size = new System.Drawing.Size(1075, 494);
             this.tpBudget.TabIndex = 3;
             this.tpBudget.Text = "Budget/Employees";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(336, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(423, 41);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Budget / Total Employes";
             // 
             // btnNext
             // 
@@ -375,23 +415,39 @@
             this.lbDate.Text = "Date will be here";
             this.lbDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // listView1
+            // lblTotalBudget
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(6, 71);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1063, 423);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.lblTotalBudget.AutoSize = true;
+            this.lblTotalBudget.BackColor = System.Drawing.Color.White;
+            this.lblTotalBudget.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalBudget.Location = new System.Drawing.Point(125, 169);
+            this.lblTotalBudget.Name = "lblTotalBudget";
+            this.lblTotalBudget.Size = new System.Drawing.Size(155, 41);
+            this.lblTotalBudget.TabIndex = 3;
+            this.lblTotalBudget.Text = "Budget :";
             // 
-            // label8
+            // lblTotalEmployees
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(481, 30);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(35, 13);
-            this.label8.TabIndex = 1;
-            this.label8.Text = "label8";
+            this.lblTotalEmployees.AutoSize = true;
+            this.lblTotalEmployees.BackColor = System.Drawing.Color.White;
+            this.lblTotalEmployees.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalEmployees.Location = new System.Drawing.Point(125, 261);
+            this.lblTotalEmployees.Name = "lblTotalEmployees";
+            this.lblTotalEmployees.Size = new System.Drawing.Size(308, 41);
+            this.lblTotalEmployees.TabIndex = 3;
+            this.lblTotalEmployees.Text = "Total Employees :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 4;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(77, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(929, 214);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -417,6 +473,8 @@
             this.groupBox1.PerformLayout();
             this.tpEmployeeList.ResumeLayout(false);
             this.tpEmployeeList.PerformLayout();
+            this.tpBudget.ResumeLayout(false);
+            this.tpBudget.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.epMenu)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -451,10 +509,14 @@
         private System.Windows.Forms.Button btnAddEmployee;
         private System.Windows.Forms.PictureBox pbIdentity;
         private System.Windows.Forms.ErrorProvider epMenu;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label lbDate;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTotalEmployees;
+        private System.Windows.Forms.Label lblTotalBudget;
+        private System.Windows.Forms.Button button1;
     }
 }
 
